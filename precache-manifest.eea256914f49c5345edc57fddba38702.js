abddabc3e649f9f0cce5ddcd80b90c7fc4b94a33
sneha <72269836+sneha-del@users.noreply.github.com>
2021-05-17 21:45:07 +0530

    Updates

---
+++ precache-manifest.eea256914f49c5345edc57fddba38702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8b75c9271d6b3fef17c",
+    "url": "/Movie-App/static/css/main.45bfa5cd.chunk.css"
+  },
+  {
+    "revision": "e8b75c9271d6b3fef17c",
+    "url": "/Movie-App/static/js/main.abbdf1c3.chunk.js"
+  },
+  {
+    "revision": "cd3933ac7b08d6ec5b19",
+    "url": "/Movie-App/static/js/runtime~main.0c63201c.js"
+  },
+  {
+    "revision": "caf4be3712fb213fcce0",
+    "url": "/Movie-App/static/js/2.a80aa21c.chunk.js"
+  },
+  {
+    "revision": "ee609f01ae985e2ba079",
+    "url": "/Movie-App/static/js/3.af959e76.chunk.js"
+  },
+  {
+    "revision": "3634de2bf56811d616d1d4e44b44a19e",
+    "url": "/Movie-App/index.html"
+  }
+];
